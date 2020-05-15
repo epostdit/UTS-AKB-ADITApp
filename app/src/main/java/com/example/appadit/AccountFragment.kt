9d2c13ba552bb399a76c2c063bcf326db8b652c4
Aditya <35457393+epostdit@users.noreply.github.com>
2020-05-15 23:41:25 +0700

    membuat map & maker dan mengedit untuk menu video tetapi masih error

--- app/src/main/java/com/example/appadit/AccountFragment.kt
+++ app/src/main/java/com/example/appadit/AccountFragment.kt
@@ -1,10 +1,15 @@
 package com.example.appadit
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_account.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,4 +25,17 @@ class AccountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findMe.setOnClickListener{
+                val uri = Uri.parse("geo:-6.889612, 107.631851?q=6°53'22.6\"S 107°37'54.7\"E")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            try {
+                Toast.makeText(activity, "Open Maps", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("geo:-6.889612, 107.631851?q=6°53'22.6\"S 107°37'54.7\"E")))
+            }
+        }
+    }
 }
